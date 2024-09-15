372027ba96f6a8a9932e2dc46c06b1ec534ffbb8
shahzaibdeveloper098 <shahzaibweb098@gmail.com>
2024-09-15 15:19:06 +0500

    add file

---
+++ student.js
@@ -0,0 +1,59 @@
+var data=[
+    {
+        id:"1",
+        name:"Abdul Wahab Amir",
+        f_name:"Wahab Amir",
+        contact:"0334-4567890",
+        class:"VC",
+        gender:"male",
+        
+    
+    },
+
+    {
+        id:"2",
+        name:"Sana Javeed",
+        f_name:"Javeed Ahmed",
+        contact:"0333-4567890",
+        class:"VC",
+        gender:"Female",
+        
+    
+    },
+
+    {
+        id:"3",
+        name:"Sheroz Sohail",
+        f_name:"Sohail Khan",
+        contact:"0334-4321890",
+        class:"VC",
+        gender:"male",
+        
+    
+    },
+
+    {
+        id:"4",
+        name:"Rehan Ali",
+        f_name:"Khan Ali",
+        contact:"0335-3453890",
+        class:"VC",
+        gender:"male",
+        
+    
+    },
+    
+    {
+        id:"5",
+        name:"Uzair Ashfaq",
+        f_name:"Ashfaq Aziz",
+        contact:"0334-8907890",
+        class:"VC",
+        gender:"male",
+        
+    
+    },
+]
+
+
+
